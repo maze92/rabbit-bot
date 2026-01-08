bb73a6efd983844b844643e6bf06970bcbf59167
maze92 <162896786+maze92@users.noreply.github.com>
2026-01-08 01:37:05 +0000

    Add check to prevent multiple starts of game news
    Prevent multiple initializations of the game news system.

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,5 +1,16 @@
+let started = false;
+
 module.exports = (client) => {
-  client.once('clientReady', () => {
+  client.once('clientReady', async () => {
     console.log(`${client.user.tag} is online!`);
+
+    if (started) return;
+    started = true;
+
+    const gameNews = require('../systems/gamenews');
+    const config = require('../config/defaultConfig');
+
+    // Iniciar sistema de notícias automáticas
+    gameNews(client, config);
   });
 };
