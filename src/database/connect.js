c9737304633aef57afa03fde532eb1fbbca8f3bf
.mazeee1992 <162896786+maze92@users.noreply.github.com>
2026-01-12 21:22:50 +0000

    Update connect.js

--- src/database/connect.js
+++ src/database/connect.js
@@ -1,9 +1,6 @@
 const mongoose = require('mongoose');
 
 // Conecta ao MongoDB usando a URI do .env
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
+mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('✅ MongoDB connected'))
   .catch(err => console.error('❌ MongoDB connection error:', err));
