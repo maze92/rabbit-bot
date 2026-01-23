a0305ce99062657078bae0bab5fddcb8bfa19aff
.mazeee1992 <162896786+maze92@users.noreply.github.com>
2026-01-23 02:18:07 +0000

    Delete src directory

--- src/slash/utils.js
+++
@@ -1,7 +0,0 @@
-// src/slash/utils.js
-
-// Kept for backwards compatibility within slash commands.
-// Prefer importing from src/utils/staff.js in new code.
-const { isStaff } = require('../utils/staff');
-
-module.exports = { isStaff };
