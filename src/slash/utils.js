c8a1dbb20df2a91a58293687985e7be6503b052d
.mazeee1992 <162896786+maze92@users.noreply.github.com>
2026-03-01 20:28:14 +0000

    Delete src directory

--- src/slash/utils.js
+++
@@ -1,9 +0,0 @@
-// src/slash/utils.js
-
-const { isStaff, canUseTicketOrHelp, isHighStaff } = require('../utils/staff');
-
-module.exports = {
-  isStaff,
-  canUseTicketOrHelp,
-  isHighStaff
-};
--- src/utils/regex.js
+++
@@ -1,6 +0,0 @@
-function escapeRegex(text) {
-  if (text === null || text === undefined) return '';
-  return String(text).replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
-}
-
-module.exports = { escapeRegex };
